9b49abdc258269d708a6e9b9bd9af3620318bb6b
Maxime CRESPO <macrespo@e1r4p6.42.fr>
2019-10-15 18:26:12 +0200

    prout

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,21 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 12:52:04 by macrespo          #+#    #+#             */
-/*   Updated: 2019/10/15 14:08:16 by macrespo         ###   ########.fr       */
+/*   Updated: 2019/10/15 17:50:43 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+static int		str_gnl(char **line)
+{
+	int		i;
+
+}
+
 int		get_next_line(int fd, char **line)
 {
-	
+	if (fd == 1)
+		str_gnl();
+	else
+		files_gnl();
 	return (0);
 }
